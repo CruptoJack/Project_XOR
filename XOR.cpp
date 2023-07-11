4cbd94f25985fc450e5e2e4c82b0789046382e87
CryptoJack <133564401+CruptoJack@users.noreply.github.com>
2023-07-11 14:06:47 +0300

    Add files via upload

---
+++ XOR.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+string encryptDecrypt(string input,string key) {
+    string output = input;
+    for (int i = 0; i < input.size(); ++i) {
+        output[i] = input[i] ^ key[i % key.length()];
+    }
+    return output;
+}
+
+int main() {
+    string message = "Hello World!";
+    string encryptionKey = "encryption";
+
+    // Шифруем сообщение
+    string encryptedMessage = encryptDecrypt(message, encryptionKey);
+    cout << "Encrypted Message: " << encryptedMessage <<endl;
+
+    // Дешифруем сообщение
+    string decryptedMessage = encryptDecrypt(encryptedMessage, encryptionKey);
+    cout << "Decrypted Message: " << decryptedMessage << endl;
+
+}
